9e292bf92710eb116510834f346f6e67216e2910
ghozziayoub <ghozziayoub@gmail.com>
2020-12-12 17:24:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'itbs-talentlab';
+
+  //des attributs
+  public name: String = "TALENTLAB"
+  public users: String[] = ["Augustin", "Ahmed", "Rouaa", "Ameni", "Zeineb", "Mohamed"]
+
+  public product: any = {
+    id: 1,
+    name: "lait",
+    price: 1.250,
+    brand: "Délice",
+    category: {
+      id: 2,
+      name: "food"
+    }
+  }
+
+  public products: any[] = [
+    {
+      id: 1,
+      name: "lait",
+      price: 1.250,
+      brand: "Délice",
+      category: {
+        id: 2,
+        name: "food"
+      }
+    },
+    {
+      id: 2,
+      name: "Iphone 11",
+      price: 4250,
+      brand: "Apple",
+      category: {
+        id: 1,
+        name: "Smartphone"
+      }
+    }
+  ]
+
+  //constructeur
+
+  //des methodes
+  public deleteUser(username: String): void {
+    let indice = this.users.indexOf(username)
+    this.users.splice(indice, 1)
+  }
+
+
 }
